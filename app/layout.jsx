67f736e813987310278f58251ac500269ad3fb1f
Ahmet Deveci <ahmet.deveci@commencis.com>
2024-10-13 08:00:05 +0300

    feat: add navbar component

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,21 +1,20 @@
-import '@/assets/styles/global.css'
+import '@/assets/styles/global.css';
+import Navbar from '@/components/Navbar';
 
 export const metadata = {
-    title: 'Property Pulse | Find The Perfect Rental',
-    description: 'Find your dream rental property',
-    keywords: 'rental, find rentals, find properties'
-}
-const MainLayout = ({children}) => {
-    return (
-        <html lang='en'>
-            <body>
-                <div>
-                    {children}
-                </div>
-            </body>
-        </html>
-
-    );
+  title: 'Property Pulse | Find The Perfect Rental',
+  description: 'Find your dream rental property',
+  keywords: 'rental, find rentals, find properties',
+};
+const MainLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar />
+        <main>{children}</main>
+      </body>
+    </html>
+  );
 };
 
 export default MainLayout;
