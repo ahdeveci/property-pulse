5e22992be80df5c6349ca801149dab7893c40e15
Ahmet Deveci <ahmet.deveci@commencis.com>
2024-10-21 09:38:07 +0300

    Refactor property fetching logic
    Move property fetching to a reusable function in utils/requests.js. Update HomeProperties and PropertiesPage components to use the new fetch function, and sort properties by creation date in PropertiesPage.

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,35 +1,24 @@
 import PropertyCard from '@/components/PropertyCard';
-
-async function fetchProperties(){
-    try {
-        const res = await fetch(`${process.env.NEXT_PUBLIC_API_API_DOMAIN}/properties`);
-        if (!res.ok) {
-            throw new Error('Failed to fetch data');
-        }
-
-        return res.json()
-    } catch(error) {
-        console.error(error);
-    }
-}
+import {fetchProperties} from "@/utils/requests";
 
 const PropertiesPage = async () => {
     const properties = await fetchProperties();
-  return (
-    <section className="px-4 py-6">
-      <div className="container-xl lg:container m-auto px-4 py-6">
-        {properties.length === 0 ? (
-          <p>No properties found</p>
-        ) : (
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-            {properties.map((property) => (
-              <PropertyCard property={property} key={property._id} />
-            ))}
+    properties.sort((a,b) => new Date(b.createdAt) - new Date(a.createdAt));
+    return (
+        <section className="px-4 py-6">
+          <div className="container-xl lg:container m-auto px-4 py-6">
+            {properties.length === 0 ? (
+              <p>No properties found</p>
+            ) : (
+              <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                {properties.map((property) => (
+                  <PropertyCard property={property} key={property._id} />
+                ))}
+              </div>
+            )}
           </div>
-        )}
-      </div>
-    </section>
-  );
+        </section>
+      );
 };
 
 export default PropertiesPage;
---
+++ utils/requests.js
@@ -0,0 +1,12 @@
+export async function fetchProperties(){
+    try {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_API_DOMAIN}/properties`);
+        if (!res.ok) {
+            throw new Error('Failed to fetch data');
+        }
+
+        return res.json()
+    } catch(error) {
+        console.error(error);
+    }
+}
