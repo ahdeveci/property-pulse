b3f12f284a588861697dfbc7b4f686e5a64d927d
Ahmet Deveci <ahmet.deveci@commencis.com>
2024-12-25 07:33:21 +0300

    add property page

--- components/PropertyCard.jsx
+++ components/PropertyCard.jsx
@@ -56,6 +56,11 @@ const PropertyCard = ({ property }) => {
         </div>
 
         <div className="flex justify-center gap-4 text-green-900 text-sm mb-4">
+          {property.rates.nightly && (
+            <p>
+              <FaMoneyBill className="inline mr-2" /> Nightly
+            </p>
+          )}
           {property.rates.weekly && (
             <p>
               <FaMoneyBill className="inline mr-2" /> Weekly
