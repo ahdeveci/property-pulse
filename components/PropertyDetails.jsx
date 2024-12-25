0b4c1cd632acaf6a86bf90f81aa3aaedd9226944
Ahmet Deveci <ahmet.deveci@commencis.com>
2024-12-25 07:45:58 +0300

    add spinner into the property details page

--- components/PropertyDetails.jsx
+++ components/PropertyDetails.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FaLocationDot, FaXmark } from 'react-icons/fa6';
-import { FaBath, FaBed, FaRulerCombined } from 'react-icons/fa';
+import { FaBath, FaBed, FaCheck, FaRulerCombined } from 'react-icons/fa';
 
 const PropertyDetails = ({ property }) => {
   return (
@@ -76,64 +76,15 @@ const PropertyDetails = ({ property }) => {
       </div>
 
       <div className="bg-white p-6 rounded-lg shadow-md mt-6">
-        <h3 className="text-lg font-bold mb-6">Amenities</h3>
+        <h3 className="text-lg font-bold mb-4">Amenities</h3>
 
-        <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 list-none">
-          <li>
-            <i className="fas fa-check text-green-600 mr-2 mt-3"></i> Wifi
-          </li>
-          <li>
-            <i className="fas fa-check text-green-600 mr-2 mt-3"></i>
-            Full kitchen
-          </li>
-          <li>
-            <i className="fas fa-check text-green-600 mr-2 mt-3"></i>
-            Washer & Dryer
-          </li>
-          <li>
-            <i className="fas fa-check text-green-600 mr-2 mt-3"></i>
-            Free Parking
-          </li>
-          <li>
-            <i className="fas fa-check text-green-600 mr-2 mt-3"></i>
-            Hot Tub
-          </li>
-          <li>
-            <i className="fas fa-check text-green-600 mr-2 mt-3"></i>
-            24/7 Security
-          </li>
-          <li>
-            <i className="fas fa-check text-green-600 mr-2 mt-3"></i>
-            Wheelchair Accessible
-          </li>
-          <li>
-            <i className="fas fa-check text-green-600 mr-2 mt-3"></i>
-            Elevator Access
-          </li>
-          <li>
-            <i className="fas fa-check text-green-600 mr-2 mt-3"></i>
-            Dishwasher
-          </li>
-          <li>
-            <i className="fas fa-check text-green-600 mr-2 mt-3"></i>
-            Gym/Fitness Center
-          </li>
-          <li>
-            <i className="fas fa-check text-green-600 mr-2 mt-3"></i>
-            Air Conditioning
-          </li>
-          <li>
-            <i className="fas fa-check text-green-600 mr-2 mt-3"></i>
-            Balcony/Patio
-          </li>
-          <li>
-            <i className="fas fa-check text-green-600 mr-2 mt-3"></i>
-            Smart TV
-          </li>
-          <li>
-            <i className="fas fa-check text-green-600 mr-2 mt-3"></i>
-            Coffee Maker
-          </li>
+        <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 list-none space-y-2 place-items-baseline">
+          {property.amenities.map((amenity, index) => (
+            <li key={index}>
+              <FaCheck className="inline-block text-green-600 mr-2"></FaCheck>{' '}
+              {amenity}
+            </li>
+          ))}
         </ul>
       </div>
       <div className="bg-white p-6 rounded-lg shadow-md mt-6">
